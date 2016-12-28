4d3b6293c0d3a7434bf22b33610b41a86f86967b
kodomazer <brianzha@gmail.com>
2016-12-27 19:48:39 -0800

    Namespace and Directories
    Added namespace declarations to all the files (hopefully)
    Rearranged some of the directories to better reflect namespaces

--- Phalanx/Assets/Scripts/Map/Tile.cs
+++ Phalanx/Assets/Scripts/Map/Tile.cs
@@ -1,71 +1,86 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Olympus.Phalanx.Entity;
 
-public class Tile : MonoBehaviour {
+namespace Olympus.Phalanx.Map
+{
 
-
-    public Occupant occupant
-    {//TODO
-        get { return null; }
-    }
-
-    //Possibly use an enum
-    public GameConstants.terrainType terrainType
-    {//TODO
-        get { return 0; }
-    }
-
-    public int x
-    {//TODO
-        get {return 0; }
-        set { }
-    }
-    public int y
-    {//TODO
-        get { return 0; }
-        set { }
-    }
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-    public void addEffect()
+    public class Tile : MonoBehaviour
     {
-        //TODO
-    }
 
-    //Called on initialization
-    public void addNeighbor(Tile neighbor)
-    {
-        //TODO
-    }
+        bool this[Tile neighbor]
+        {
+            get
+            {
+                //TODO
+                return false;
+            }
+        }
+
+        public IOccupant occupant
+        {//TODO
+            get { return null; }
+        }
+
+        public TerrainType terrain
+        {//TODO
+            get { return null; }
+        }
+
+        public int x
+        {//TODO
+            get { return 0; }
+            set { }
+        }
+        public int y
+        {//TODO
+            get { return 0; }
+            set { }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void addEffect()
+        {
+            //TODO
+        }
+
+        //Called on initialization
+        public void addNeighbor(Tile neighbor)
+        {
+            //TODO
+        }
 
 
-    void dealDamage(AttackInfo info)
-    {
-        //TODO
-    }
+        void dealDamage(AttackInfo info)
+        {
+            //TODO
+        }
 
-    public void enterTile(Occupant newOccupant)
-    {
-        //TODO
-    }
+        public void enterTile(Entity.IOccupant newOccupant)
+        {
+            //TODO
+        }
 
-    public void exitTile()
-    {
-        //TODO
-    }
+        public void exitTile()
+        {
+            //TODO
+        }
 
-    public Tile[] getNeighbors()
-    {
-        //TODO
-        return null;
+        public Tile[] getNeighbors()
+        {
+            //TODO
+            return null;
+        }
     }
 }
