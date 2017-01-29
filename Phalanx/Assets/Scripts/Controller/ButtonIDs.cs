a597a7f388c343df801f440dd0633ba2bef1c28d
RonNofar <ronnofar2@gmail.com>
2017-01-28 16:55:34 -0800

    Button IDs Added
    Added button IDs to ButtonIDs.cs

--- Phalanx/Assets/Scripts/Controller/ButtonIDs.cs
+++ Phalanx/Assets/Scripts/Controller/ButtonIDs.cs
@@ -7,6 +7,9 @@ namespace Olympus.Phalanx.Controller
     public enum ButtonID
     {
         Debug = 0x1000,
-        Action = 0x1001
+        Action = 0x1001,
+        Move = 0x1002,
+        Info = 0x1003,
+        Skill = 0x1004 // For future
     }
 }
